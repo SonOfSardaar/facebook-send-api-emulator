b0b930c2f216ae1da6a76fc34996213f07b6c07f
Gurpreet <gurpreet@nimbleit.solutions>
2017-11-08 23:35:04 +0000

    ui working with bundles

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const excludePaths = /(node_modules|bower_components)/;
 
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-let sassLoader = new ExtractTextPlugin("app.min.css");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const contextPath = path.join(__dirname, "./ui")
 const distPath = path.join(__dirname, "./dist")
 
@@ -25,23 +24,19 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.scss$/,
-                exclude: excludePaths,
-                loader: sassLoader.extract("css-loader!sass-loader")
-            }, {
                 test: /\.css$/,
-                loader: "style-loader!css-loader"
+                use: ExtractTextPlugin.extract({fallback: "style-loader", use: "css-loader"})
             }, {
-                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-                loader: 'url-loader?limit=100000'
+                test: /\.(woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=10240&name=/fonts/[name].[ext]'
             }, {
-                test: /\.(jpe?g|gif|png|svg|woff|ttf|wav|mp3|ico)$/,
-                loader: "file-loader?name=./images/[name].[ext]"
+                test: /\.(jpe?g|gif|png)$/,
+                loader: "file-loader?name=/images/[name].[ext]"
             }
         ]
     },
     plugins: [
-        sassLoader,
+        new ExtractTextPlugin("css/styles.min.css"),
         new HtmlWebpackPlugin({
             template: "./index.html",
             files: {
