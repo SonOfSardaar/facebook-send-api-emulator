b9c98c6f3855aeead00247c6a42c46cdef421434
Gurpreet <gurpreet.singh@asos.com>
2017-11-14 11:40:04 +0000

    added ability to set configuration via api

--- api/data/config.js
+++ api/data/config.js
@@ -1,9 +1,16 @@
-const webServerPort= process.env["SendApi.Port"] || 3000;
+const webServerPort = process.env["SendApi.Port"] || 3000;
+
+const config = require("../services/database").getData("configuration") || {
+    webHookUrl: `http://localhost:${webServerPort}/webhook`,
+    pageScopeId: "SET_AS_ENV_VAR",
+    appSecret: "SET_AS_ENV_VAR",
+    pageAccessToken : "SET_AS_ENV_VAR"
+};
 
 module.exports = {
-    webHookUrl: process.env["SendApi.WebHookUrl"] || `http://localhost:${webServerPort}/webhook`,
-    pageScopeId: process.env["SendApi.PageScopeId"] || "SET AS ENV VAR",
-    appSecret: process.env["SendApi.AppSecret"] || "SET AS ENV VAR",
+    webHookUrl: process.env["SendApi.WebHookUrl"] || config.webHookUrl,
+    pageScopeId: process.env["SendApi.PageScopeId"] || config.pageScopeId,
+    appSecret: process.env["SendApi.AppSecret"] || config.appSecret,
     port: webServerPort,
-    pageAccessToken:"XXXXXXXXXXXXX"
+    pageAccessToken: config.pageAccessToken
 };
